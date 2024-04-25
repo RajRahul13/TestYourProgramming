8a5e5ae6a7eecff59b0b443e6d7846ef9465f46c
Rahul Raj <raj.rahul1213@gmail.com>
2024-04-25 17:49:33 +0530

    new changes

--- TestYourProgramming/src/test/programming/string/SwapString.java
+++ TestYourProgramming/src/test/programming/string/SwapString.java
@@ -7,13 +7,15 @@ public class SwapString {
 		System.out.println(str1+str2);
 		str1=str1+str2; //step1
 		
-		System.out.println(str1.length());
-		System.out.println(str2.length());
+		/*
+		 * System.out.println(str1.length()); System.out.println(str2.length());
+		 */
 		
 		str2=str1.substring(0,(str1.length()-str2.length())); //step2
 		
 		str1=str1.substring(str2.length()); //step3
 		
+		System.out.println(str1);
 		System.out.println(str2);
 	}
 }
---
+++ TestYourProgramming/src/test/programming/stringBuildervsBuffer.java
@@ -0,0 +1,35 @@
+package test.programming;
+
+public class stringBuildervsBuffer {
+	
+	public static void main (String [] args) {
+	//	StringBuilder sb = new StringBuilder(); // not threadsafe so fast
+		StringBuffer sb = new StringBuffer(); // threadsafe slower than builder
+		
+		Thread thread1 = new Thread(() -> {
+			for(int i=0;i<1000;i++) {
+				sb.append("A");
+			}
+		});
+		
+		Thread thread2 = new Thread(() -> {
+			for(int i=0;i<1000;i++) {
+				sb.append("B");
+			}
+		});
+		
+		thread1.start();
+		thread2.start();
+		
+		try {
+			thread1.join();
+			thread2.join();
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		
+	//	System.out.println("Result (String Builder):"+ sb.toString().length());
+		System.out.println("Result (String Buffer):"+ sb.toString().length());
+;
+		} 
+}
